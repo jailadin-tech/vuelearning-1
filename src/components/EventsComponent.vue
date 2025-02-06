9ce012d9adc0eeaeb5e4f03e1654862000d4793e
jailadin <jailadinshaik@gmail.com>
2025-02-06 19:36:29 +0530

    Event Handlilng

---
+++ src/components/EventsComponent.vue
@@ -0,0 +1,29 @@
+<script setup>
+/* Counting */
+import { ref } from 'vue'
+const count = ref(0)
+/* show Message */
+const showmessage = (message) => {
+  alert(message + '' + count.value)
+}
+/* Form handling */
+const formHandler = (e) => {
+  e.preventDefault()
+  alert('form submited')
+}
+</script>
+<template>
+  <h4>Count: {{ count }}</h4>
+  <button v-on:click="count++">Increment</button>
+  <button v-on:click="count--">Decrement</button>
+  <br />
+  <br />
+  <button @:click="showmessage('Current Counting is: ')">ShowMessage</button>
+  <br />
+  <br />
+  <form action="" v-on:submit="formHandler">
+    <input type="text" name="UserName" id="" />
+    <input type="text" name="PhoneNo" id="" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
