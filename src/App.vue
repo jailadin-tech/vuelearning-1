2101a0a55354be9d51531e4eabc28a7094b32233
jailadin <jailadinshaik@gmail.com>
2025-02-06 19:57:53 +0530

    Reactive actions

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import MyComponent from './components/MyComponent.vue'
 import ModuleStyling from './components/ModuleStyling.vue'
 import EventComponent from './components/EventsComponent.vue'
+import ReactiveComponent from './components/ReactiveComponent.vue'
 </script>
 
 <template>
   <!-- <MyComponent/> -->
   <!-- <ModuleStyling /> -->
-  <EventComponent />
+  <!--  <EventComponent /> -->
+  <ReactiveComponent />
 </template>
 
 <style scoped></style>
