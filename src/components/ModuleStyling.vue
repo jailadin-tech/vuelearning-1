e6de378deeace27aa2b8f32e404d992b1a637028
jailadin <jailadinshaik@gmail.com>
2025-02-06 18:51:55 +0530

    module styleing

---
+++ src/components/ModuleStyling.vue
@@ -0,0 +1,21 @@
+<template>
+  <!--   <h1 class="local-style">Local Styling the element</h1>-->
+  <!-- <h2 :class="$style['mystyle']">Module styling</h2> -->
+  <h2 :class="myclasses.mystyle">Module styling</h2>
+</template>
+
+<!-- Module styling -->
+<style module="myclasses">
+.mystyle {
+  background-color: seagreen;
+  color: wheat;
+}
+</style>
+
+<!-- Localstyle  -->
+<!-- <style scoped>
+.local-style {
+  background-color: firebrick;
+  color: wheat;
+}
+</style> -->
