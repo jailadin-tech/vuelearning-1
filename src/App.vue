4eb08472f85af47de609106f2cacbc05d6c2841e
jailadin <jailadinshaik@gmail.com>
2025-02-06 19:35:56 +0530

    Event Handling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MyComponent from './components/MyComponent.vue'
 import ModuleStyling from './components/ModuleStyling.vue'
+import EventComponent from './components/EventsComponent.vue'
 </script>
 
 <template>
   <!-- <MyComponent/> -->
-  <ModuleStyling />
+  <!-- <ModuleStyling /> -->
+  <EventComponent />
 </template>
 
 <style scoped></style>
